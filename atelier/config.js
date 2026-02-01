04c9388dd5ed6a376b46bebd309513a402678b94
damien pichard <magvl4gleize@durandservices.fr>
2026-02-01 11:31:13 +0100

    Add files via upload

--- atelier/config.js
+++ atelier/config.js
@@ -1,6 +1,3 @@
-// Fichier servi en statique par nginx
-// Les pages Atelier attendent window.__ATELIER_CFG.GS_URL
 window.__ATELIER_CFG = {
-  // Cette URL DOIT retourner directement du JSON: { lignes: [...], regles: [...] }
   GS_URL: "/atelier/api/config"
 };
